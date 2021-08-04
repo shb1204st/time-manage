a0f55554cb204e8d0c0299fc62bae2719603f125
shb1204st <shb1204st@gmail.com>
2021-08-04 21:44:51 +0900

    ユーザー管理　テストコード6
    factrybotの編集

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+      person { Gimei.name }
+    end
+    email                 { Faker::Internet.free_email }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    name                  { person.kanji }
+    kana_name             { person.katakana }
+    department            { Faker::jobTitle.name }
+  end
+end
