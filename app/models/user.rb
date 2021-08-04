4df5acfe021a2e572801cd3d1a781f2f19a31712
shb1204st <shb1204st@gmail.com>
2021-08-04 14:27:23 +0900

    ユーザー管理　ビューの設定
    正規表現を記述し、各項目に制限をかける。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name,       format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :kana_name,  format: { with:/\A[ァ-ヶー]+\z/ }
+    validates :department
+    validates :password,   format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  end
 end
