5fa39c3b2e6f534fd11d67f68402a8252b5faa71
shb1204st <shb1204st@gmail.com>
2021-08-05 15:51:39 +0900

    時間内容機能　コントローラー編集
    createアクション作成

--- app/controllers/time_content_controller.rb
+++ app/controllers/time_content_controller.rb
@@ -7,4 +7,15 @@ class TimeContentController < ApplicationController
     @time_content = TimeContent.new
   end
 
+  def create
+    TimeContent.create(time_content_parameter)
+    redirect_to root_path
+  end
+
+  private
+
+  def time_content_parameter
+    params.require(:time_content).permit(:start_time, :time_id, :content_id, :detail )
+  end
+
 end
