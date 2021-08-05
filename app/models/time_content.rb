bf2ad601f3aa18b71d04e6d74fe4545fe86fef73
shb1204st <shb1204st@gmail.com>
2021-08-05 12:22:23 +0900

    時間内容機能　モデルの編集
    アソシエーション設定
    バリデーションの設定

--- app/models/time_content.rb
+++ app/models/time_content.rb
@@ -1,2 +1,13 @@
 class TimeContent < ApplicationRecord
+  belons_to :user
+
+  with_options numericality: { other_than: 1, message: "is invalid" } do
+   validates :time_id
+   validates :content_id
+  end
+
+  with_options presence: true do
+   validates :detail
+   validates :start_time 
+  end
 end
