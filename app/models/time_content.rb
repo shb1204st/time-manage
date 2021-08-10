b5aba961240e2ff48141f0cad1f24279c604475c
shb1204st <shb1204st@gmail.com>
2021-08-10 11:58:09 +0900

    円グラフ表示　表示モデル作成

--- app/models/time_content.rb
+++ app/models/time_content.rb
@@ -3,6 +3,20 @@ class TimeContent < ApplicationRecord
   belongs_to :time_step
   belongs_to :content
 
+    def self.content_data
+        @time_contents = TimeContent.all
+        content_graph_data = {}
+        @time_contents.each_with_index do |time_content, index|
+          content_graph_data = { time_content.content.name => 0 } if index.zero?
+          if content_graph_data.key?(time_content.content.name)
+            content_graph_data[time_content.content.name] += 1
+          else
+            content_graph_data[time_content.content.name] = 1
+          end
+        end
+        content_graph_data
+    end
+  
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :time_step_id
     validates :content_id
@@ -14,4 +28,5 @@ class TimeContent < ApplicationRecord
   end
 
   belongs_to :user
+
 end
