f574a3915583b842f9b92bb91e8bc29223366764
shb1204st <shb1204st@gmail.com>
2021-08-13 20:36:14 +0900

    ルボコップ実施

--- app/models/time_content.rb
+++ app/models/time_content.rb
@@ -5,17 +5,17 @@ class TimeContent < ApplicationRecord
 
   class << self
     def content_data
-        @time_contents = TimeContent.all
-        content_graph_data = {}
-        @time_contents.each_with_index do |time_content, index|
-          content_graph_data = { time_content.content.name => 0 } if index.zero?
-          if content_graph_data.key?(time_content.content.name)
-            content_graph_data[time_content.content.name] += 1
-          else
-            content_graph_data[time_content.content.name] = 1
-          end
+      @time_contents = TimeContent.all
+      content_graph_data = {}
+      @time_contents.each_with_index do |time_content, index|
+        content_graph_data = { time_content.content.name => 0 } if index.zero?
+        if content_graph_data.key?(time_content.content.name)
+          content_graph_data[time_content.content.name] += 1
+        else
+          content_graph_data[time_content.content.name] = 1
         end
-        content_graph_data
+      end
+      content_graph_data
     end
 
     def background_color_content_data
@@ -37,8 +37,8 @@ class TimeContent < ApplicationRecord
       background_colors
     end
   end
-  
-  with_options numericality: { other_than: 1, message: "を選択してください"} do
+
+  with_options numericality: { other_than: 1, message: 'を選択してください' } do
     validates :time_step_id
     validates :content_id
   end
@@ -49,5 +49,4 @@ class TimeContent < ApplicationRecord
   end
 
   belongs_to :user
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :name,       format: { with: /\A[ぁ-んァ-ヶ一-龥々ー ]+\z/, message: "は全角の漢字もしくはひらがなもしくはカタカナで入力してください"}
-    validates :kana_name,  format: { with: /\A[ァ-ヶー ]+\z/,message: "は全角のカタカナで入力してください" }
+    validates :name,       format: { with: /\A[ぁ-んァ-ヶ一-龥々ー ]+\z/, message: 'は全角の漢字もしくはひらがなもしくはカタカナで入力してください' }
+    validates :kana_name,  format: { with: /\A[ァ-ヶー ]+\z/, message: 'は全角のカタカナで入力してください' }
     validates :department
   end
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "は半角英数字で入力してください"  }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字で入力してください' }
 
   has_many :time_contents
 end
