b1814bec2a494edffd15af1c305d580fdfdd7f93
shb1204st <shb1204st@gmail.com>
2021-08-23 15:10:50 +0900

    リファクタリング　ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     get '/users', to: 'devise/registrations#new'
+    post '/users/guest_sign_in', to: 'users#guest_sign_in'
   end
   root to: "time_content#index"
-  post '/time_content/guest_sign_in', to: 'time_content#guest_sign_in'
   resources :users, only: [:show]
   resources :time_content do
     collection do
      get 'keyword_search', 'detail_search'
     end
   end
-  
+
 end
