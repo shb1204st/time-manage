e8e183dee57337175b8564570377aa7f628ed409
shb1204st <shb1204st@gmail.com>
2021-08-24 08:08:27 +0900

    カラムの追加
    ターミナルにてrollbackを実施し以前作成したカラム追加ファイルを編集

--- db/migrate/20210813233040_change_time_contents_columns.rb
+++ db/migrate/20210813233040_change_time_contents_columns.rb
@@ -1,5 +1,6 @@
 class ChangeTimeContentsColumns < ActiveRecord::Migration[6.0]
   def change
     add_column :time_contents, :availability, :boolean, default: false, null: false
+    add_column :time_contents, :ensure_id, :integer, null: false
   end
 end
