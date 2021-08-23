99feb360960be03b5b97bcf7bbefaa224e510c7a
shb1204st <shb1204st@gmail.com>
2021-08-23 15:11:23 +0900

    リファクタリング　コントローラー編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :guest_sign_in
 
-  def show
-    @user = User.find(params[:id])
+  def guest_sign_in
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = 'ゲスト'
+      user.kana_name = 'ゲスト'
+      user.department = '社長'
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
+  
 end
