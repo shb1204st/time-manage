60add8ad42f909727b4cb319f1f82dd18da270ba
shb1204st <shb1204st@gmail.com>
2021-08-24 14:03:44 +0900

    コントローラー編集

--- app/controllers/time_content_controller.rb
+++ app/controllers/time_content_controller.rb
@@ -8,28 +8,29 @@ class TimeContentController < ApplicationController
     @time_contents = TimeContent.where(user_id: current_user)
     # @time_contents = TimeContent.all #1つのカレンダーでユーザー全員の内容を閲覧
     @day = Date.today
-    @week = Date.today.all_week
     @month = Date.today.all_month
-    @last_month = Date.today.last_month.all_month
 
     @content_month_graph_data = TimeContent.where(user_id: current_user, start_time: @month).order('content_id ASC').content_data
     @background_month_colors = TimeContent.where(user_id: current_user,
                                                  start_time: @month).order('content_id ASC').background_color_content_data
-    @content_last_month_graph_data = TimeContent.where(user_id: current_user,
-                                                       start_time: @last_month).order('content_id ASC').content_data
-    @background_last_month_colors = TimeContent.where(user_id: current_user,
-                                                      start_time: @last_month).order('content_id ASC').background_color_content_data
-    @content_week_graph_data = TimeContent.where(user_id: current_user, start_time: @week).order('content_id ASC').content_data
-    @background_week_colors = TimeContent.where(user_id: current_user,
-                                                start_time: @week).order('content_id ASC').background_color_content_data
+
+    @content_month_reduce_graph_data = TimeContent.where(user_id: current_user, ensure_id: 3,
+                                                       start_time: @month).order('content_id ASC').content_data
+    @background_month_reduce_colors = TimeContent.where(user_id: current_user, ensure_id: 3,
+                                                      start_time: @month).order('content_id ASC').background_color_content_data
+
+    @content_day_reduce_graph_data = TimeContent.where(user_id: current_user, ensure_id: 3, start_time: @day).order('content_id ASC').content_data
+    @background_day_reduce_colors = TimeContent.where(user_id: current_user, ensure_id: 3,
+                                                start_time: @day).order('content_id ASC').background_color_content_data
+
     @content_day_graph_data = TimeContent.where(user_id: current_user, start_time: @day).order('content_id ASC').content_data
     @background_colors = TimeContent.where(user_id: current_user,
                                            start_time: @day).order('content_id ASC').background_color_content_data
 
     @day_total_time = TimeContent.where(user_id: current_user, start_time: @day).order('content_id ASC').content_data
-    @week_total_time = TimeContent.where(user_id: current_user, start_time: @week).order('content_id ASC').content_data
-    @last_month_total_time = TimeContent.where(user_id: current_user,
-                                               start_time: @last_month).order('content_id ASC').content_data
+    @day_reduce_total_time = TimeContent.where(user_id: current_user,  ensure_id: 3, start_time: @day).order('content_id ASC').content_data
+    @month_reduce_total_time = TimeContent.where(user_id: current_user,  ensure_id: 3,
+                                               start_time: @month).order('content_id ASC').content_data
     @month_total_time = TimeContent.where(user_id: current_user, start_time: @month).order('content_id ASC').content_data
   end
 
@@ -78,7 +79,7 @@ class TimeContentController < ApplicationController
   private
 
   def time_content_params
-    params.require(:time_content).permit(:start_time, :time_step_id, :content_id, :ensure, :detail).merge(user_id: current_user.id)
+    params.require(:time_content).permit(:start_time, :time_step_id, :content_id, :ensure_id, :detail).merge(user_id: current_user.id)
   end
 
   def set_time_content
