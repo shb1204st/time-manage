1cf5d30b53b263c9d5b961642f97f3b4736e7f93
shb1204st <shb1204st@gmail.com>
2021-08-25 16:24:40 +0900

    カラムの追加

--- db/migrate/20210805023350_create_time_contents.rb
+++ db/migrate/20210805023350_create_time_contents.rb
@@ -5,6 +5,7 @@ class CreateTimeContents < ActiveRecord::Migration[6.0]
       t.integer     :content_id,   null: false
       t.text        :detail,      null: false
       t.date        :start_time,  null: false
+      t.integer     :ensure_id,   null: false
       t.references  :user,        null: false, foreign_key: true
 
       t.timestamps
