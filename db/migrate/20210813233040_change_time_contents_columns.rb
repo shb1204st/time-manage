f4ae3a4d459fa4b1f572e1ada491048730086a29
shb1204st <shb1204st@gmail.com>
2021-08-25 16:24:31 +0900

    旧のカラム追加ファイルを削除
    ファイル追加は必ず、ファイルをrollback等でダウンさせてから、削除する

--- db/migrate/20210813233040_change_time_contents_columns.rb
+++
@@ -1,6 +0,0 @@
-class ChangeTimeContentsColumns < ActiveRecord::Migration[6.0]
-  def change
-    add_column :time_contents, :availability, :boolean, default: false, null: false
-    add_column :time_contents, :ensure_id, :integer, null: false
-  end
-end
--- db/migrate/20210817022655_remove_name_from_time_contents.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNameFromTimeContents < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :time_contents, :availability, :boolean
-  end
-end
