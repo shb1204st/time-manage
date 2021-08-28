da219805f74c538fb89fa300a880717a296a5ee8
shb1204st <shb1204st@gmail.com>
2021-08-28 15:39:26 +0900

    カラムの追加と削除
    time_stepを削除しbegin_timeとfinish_timeを追加　カラム型は　time

--- db/migrate/20210805023350_create_time_contents.rb
+++ db/migrate/20210805023350_create_time_contents.rb
@@ -1,7 +1,8 @@
 class CreateTimeContents < ActiveRecord::Migration[6.0]
   def change
     create_table :time_contents do |t|
-      t.integer     :time_step_id,     null: false
+      t.time        :begin_time, null: false
+      t.time        :finish_time,    null: false
       t.integer     :content_id,   null: false
       t.text        :detail,      null: false
       t.date        :start_time,  null: false
